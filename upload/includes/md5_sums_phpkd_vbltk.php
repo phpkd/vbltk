cb6143a8cb3424f2e035eae9bfabe08c2be64f58
Omranic <me@omranic.com>
2010-04-03 01:43:15 +0000

    First inistantiation, transliteration partialialy implemented, other features still not implemented yet. Transliteration implemented & working well with vB4. Docs still not reviewed at all!!

---
+++ upload/includes/md5_sums_phpkd_vbltk.php
@@ -0,0 +1,37 @@
+<?php
+/*==================================================================================*\
+|| ################################################################################ ||
+|| # Product Name: PHPKD - Advanced Quick Reply                  Version: 4.0.110 # ||
+|| # License Type: Free License                                    $Revision: 18 $ # ||
+|| # ---------------------------------------------------------------------------- # ||
+|| # 																			  # ||
+|| #            Copyright ©2005-2010 PHP KingDom. All Rights Reserved.            # ||
+|| #     This product may be redistributed in whole or significant part under     # ||
+|| #        "Creative Commons - Attribution-Noncommercial-Share Alike 3.0"        # ||
+|| # 																			  # ||
+|| # ----------------- 'Advanced Quick Reply' IS FREE SOFTWARE ------------------ # ||
+|| #        http://www.phpkd.net | http://info.phpkd.net/en/license/free          # ||
+|| ################################################################################ ||
+\*==================================================================================*/
+
+
+$md5_sums = array(
+	'/clientscript' => array(
+		'phpkd_vbaqr.js' => 'fe8fb56b6eee7eb18886873aee064b42',
+	),
+	'/includes/xml' => array(
+		'bitfield_phpkd_vbaqr.xml' => '311b1a748dae604ca8685e9909e0c9f2',
+	),
+);
+$md5_sum_softwareid = 'phpkd_vbaqr';
+$md5_sum_versions['phpkd_vbaqr'] = '4.0.110';
+
+
+/*============================================================================*\
+|| ########################################################################### ||
+|| # Version: 4.0.110
+|| # $Revision: 18 $
+|| # Released: $Date: 2010-03-23 12:34:50 +0200 (Tue, 23 Mar 2010) $
+|| ########################################################################### ||
+\*============================================================================*/
+?>
